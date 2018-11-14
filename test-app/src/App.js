d3e9d591f18ef51c85cca59d95b6d6b90ed96642
henos tekle <mk22005@hotmail.com>
2018-11-14 16:44:01 +0100

    componets
    adding componets and some source images

--- test-app/src/App.js
+++ test-app/src/App.js
@@ -1,17 +1,21 @@
 import React, { Component } from "react";
+import { Navbar, NavbarBrand } from "reactstrap";
+import Menu from './components/MenuComponent';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
-import { Navbar, NavbarBrand } from "reactstrap";
+
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div>
         <Navbar dark color="primary">
           <div className="container">
             <NavbarBrand href="/"> Ristorante Con Fusion</NavbarBrand>
           </div>
-        </Navbar>        
+        </Navbar>       
+        <Menu /> 
       </div>
     );
   }
