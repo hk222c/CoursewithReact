b28484b5df9ad9d4529bf716c068585adf783dc8
henos tekle <mk22005@hotmail.com>
2018-11-16 21:15:28 +0100

    Assigement
    Completed

--- test-app/src/components/DishdetailComponent.js
+++ test-app/src/components/DishdetailComponent.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
+import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
 
 
 export class DishdetailComponent extends Component {
@@ -12,16 +12,36 @@ export class DishdetailComponent extends Component {
    
 
     renderDish(dish) {
-        if (dish != null) {
-            console.log(dish);
+        if (dish != null) {            
+            const dishComments = dish.comments.map((aComment) => {
+                return (
+                    <div key={aComment.id}>
+                        <CardText>{aComment.comment}</CardText>
+                        <CardText>-- {aComment.author}, {aComment.date.split("T", 1)}</CardText>
+                        <br/>
+                    </div>
+                )
+            });
             return(
-                <Card>
-                    <CardImg width="100%" src={dish.image} alt={dish.name} />
-                    <CardBody>
-                        <CardTitle>{dish.name}</CardTitle>
-                        <CardText>{dish.description}</CardText>
-                    </CardBody>
-                </Card>
+                <div className="row">
+                    <div className="col-12 col-md-5">
+                        <Card>
+                            <CardImg width="100%" src={dish.image} alt={dish.name} />
+                            <CardBody>
+                                <CardTitle>{dish.name}</CardTitle>
+                                <CardText>{dish.description}</CardText>
+                            </CardBody>
+                        </Card>                    
+                    </div>                    
+                    <div className="col-12 col-md-5">
+                        <Card>
+                            <CardBody>
+                                <CardTitle>Comments</CardTitle>
+                                {dishComments}                        
+                            </CardBody>                    
+                        </Card>
+                    </div>
+                </div>             
             );
         } else {
             return(
@@ -35,7 +55,7 @@ export class DishdetailComponent extends Component {
 
     render() {
         return (
-        <div>
+        <div className="col-12 col-md-12">
             {this.renderDish(this.props.selectedDish)}            
         </div>
         )
