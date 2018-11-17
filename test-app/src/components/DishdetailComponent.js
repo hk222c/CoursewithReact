67c6b8af52d6faf0a9db065dd484506abffeeb79
henos tekle <mk22005@hotmail.com>
2018-11-17 02:37:44 +0100

    More about components
    presentation/view and container/data components

--- test-app/src/components/DishdetailComponent.js
+++ test-app/src/components/DishdetailComponent.js
@@ -4,6 +4,7 @@ import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
 
 export class DishdetailComponent extends Component {
 
+    // eslint-disable-next-line no-useless-constructor
     constructor(props) {
         super(props);        
         
@@ -12,19 +13,20 @@ export class DishdetailComponent extends Component {
    
 
     renderDish(dish) {
+        console.log(dish);
         if (dish != null) {            
             const dishComments = dish.comments.map((aComment) => {
                 return (
                     <div key={aComment.id}>
                         <CardText>{aComment.comment}</CardText>
-                        <CardText>-- {aComment.author}, {aComment.date.split("T", 1)}</CardText>
+                        <CardText>-- {aComment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day:'2-digit' }).format(new Date(Date.parse(aComment.date)))}</CardText>
                         <br/>
                     </div>
                 )
             });
             return(
                 <div className="row">
-                    <div className="col-12 col-md-5">
+                    <div className="col-12 col-md-5 m-1">
                         <Card>
                             <CardImg width="100%" src={dish.image} alt={dish.name} />
                             <CardBody>
@@ -55,8 +57,8 @@ export class DishdetailComponent extends Component {
 
     render() {
         return (
-        <div className="col-12 col-md-12">
-            {this.renderDish(this.props.selectedDish)}            
+        <div className="container">
+            {this.renderDish(this.props.dish)}            
         </div>
         )
     }
