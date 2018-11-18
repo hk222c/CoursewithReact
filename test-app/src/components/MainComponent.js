a11be91dc558ad403f049b8af5278394a970245b
henos tekle <mk22005@hotmail.com>
2018-11-18 02:43:45 +0100

    More components
    adding header and footer components

--- test-app/src/components/MainComponent.js
+++ test-app/src/components/MainComponent.js
@@ -1,8 +1,10 @@
 import React, { Component } from "react";
-import { Navbar, NavbarBrand } from "reactstrap";
+
 import Menu from './MenuComponent';
 import "bootstrap/dist/css/bootstrap.min.css";
 import DishdetailComponent from './DishdetailComponent';
+import Header from './HeaderComponent';
+import Footer from './FooterComponent';
 import { DISHES } from '../shared/dishes';
 
 class Main extends Component {
@@ -22,16 +24,13 @@ class Main extends Component {
 
   render() {
     return (
-      <div>
-        <Navbar dark color="primary">
-          <div className="container">
-            <NavbarBrand href="/"> Ristorante Con Fusion</NavbarBrand>
-          </div>
-        </Navbar>       
+      <div>     
+        <Header />  
         <Menu dishes={this.state.dishes}
             onClick={(dishId) => this.onDishSelect(dishId)} /> 
         <DishdetailComponent 
             dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish )[0]} />
+        <Footer />
       </div>
     );
   }
