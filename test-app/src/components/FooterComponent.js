73e559225fab45b2b2285da225baa9c9b5dfe964
henos tekle <mk22005@hotmail.com>
2018-11-18 02:50:50 +0100

    footer
    little more padding between social-icon for more better UI

--- test-app/src/components/FooterComponent.js
+++ test-app/src/components/FooterComponent.js
@@ -29,22 +29,22 @@ function Footer(props) {
                     </div>
                     <div className="col-12 col-sm-4 align-self-center">
                         <div className="text-center">
-                            <a className="btn btn-social-icon btn-google" href="http
+                            <a className="btn btn-social-icon btn-google m-1" href="http
                                 ://google.com/+"><i className="fa fa-google-plus"></i>
                             </a>
-                            <a className="btn btn-social-icon btn-facebook" href="http
+                            <a className="btn btn-social-icon btn-facebook m-1" href="http
                             ://www.facebook.com/profile.php?id=">
                             <i className="fa fa-facebook"></i>
                             </a>
-                            <a className="btn btn-social-icon btn-linkedin" href="http
+                            <a className="btn btn-social-icon btn-linkedin m-1" href="http
                             ://www.linkedin.com/in/"><i className="fa fa-linkedin"></i>
                             </a>
-                            <a className="btn btn-social-icon btn-twitter" href="http
+                            <a className="btn btn-social-icon btn-twitter m-1" href="http
                             ://twitter.com/"><i className="fa fa-twitter"></i>
                             </a>
-                            <a className="btn btn-social-icon btn-google" href="http
+                            <a className="btn btn-social-icon btn-google m-1" href="http
                             ://youtube.com/"><i className="fa fa-youtube"></i></a>
-                            <a className="btn btn-social-icon" href="mailto:">
+                            <a className="btn btn-social-icon m-1" href="mailto:">
                             <i className="fa fa-envelope-o"></i>
                             </a>
                         </div>
