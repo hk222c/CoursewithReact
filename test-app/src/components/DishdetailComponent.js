ad0b96aa8699d330f53a6ddb9c8473e4873b84e4
henos tekle <mk22005@hotmail.com>
2018-11-19 21:51:14 +0100

    Single page application
    more SPA and split the comments from the dish detail.

--- test-app/src/components/DishdetailComponent.js
+++ test-app/src/components/DishdetailComponent.js
@@ -1,38 +1,20 @@
 import React from 'react'
-import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
+import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 
-    function RenderDish({dish}) {        
-        if (dish != null) {            
-            const dishComments = dish.comments.map((aComment) => {
-                return (
-                    <div key={aComment.id}>
-                        <CardText>{aComment.comment}</CardText>
-                        <CardText>-- {aComment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day:'2-digit' }).format(new Date(Date.parse(aComment.date)))}</CardText>
-                        <br/>
-                    </div>
-                )
-            });
-            return(
-                <div className="row">
-                    <div className="col-12 col-md-5 m-1">
-                        <Card>
-                            <CardImg width="100%" src={dish.image} alt={dish.name} />
-                            <CardBody>
-                                <CardTitle>{dish.name}</CardTitle>
-                                <CardText>{dish.description}</CardText>
-                            </CardBody>
-                        </Card>                    
-                    </div>                    
-                    <div className="col-12 col-md-5">
-                        <Card>
-                            <CardBody>
-                                <CardTitle>Comments</CardTitle>
-                                {dishComments}                        
-                            </CardBody>                    
-                        </Card>
-                    </div>
-                </div>             
+    function RenderDish({dish}, {comments}) {        
+        if (dish != null) {           
+            return(                
+                <div className="col-12 col-md-5 m-1">
+                    <Card>
+                        <CardImg width="100%" src={dish.image} alt={dish.name} />
+                        <CardBody>
+                            <CardTitle>{dish.name}</CardTitle>
+                            <CardText>{dish.description}</CardText>
+                         </CardBody>
+                    </Card>                    
+                 </div>                             
             );
         } else {
             return(
@@ -43,10 +25,53 @@ import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
 
     } 
 
+    function RenderComments({comments}) {
+        if (comments != null) {            
+            const dishComments = comments.map((aComment) => {
+                return (
+                    <div key={aComment.id}>
+                        <CardText>{aComment.comment}</CardText>
+                        <CardText>-- {aComment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day:'2-digit' }).format(new Date(Date.parse(aComment.date)))}</CardText>
+                        <br/>
+                    </div>
+                )
+            });
+            return(
+                <div className="col-12 col-md-5">
+                        <Card>
+                            <CardBody>
+                                <CardTitle>Comments</CardTitle>
+                                {dishComments}                        
+                            </CardBody>                    
+                        </Card>
+                </div>
+            );
+        }else {
+            return(
+                <div></div>
+            );            
+        }
+
+    }
+
     const DishdetailComponent = (props) => {
         return (
             <div className="container">
-                <RenderDish dish={props.dish} />          
+                <div className="row">
+                    <Breadcrumb>                        
+                        <BreadcrumbItem><Link to='/menu'>Menu</Link></BreadcrumbItem>
+                        <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>                        
+                    </Breadcrumb>
+                    <div className="col-12">
+                        <h3>{props.dish.name}</h3>
+                        <hr />
+                    </div>
+                </div>
+                <div className="row">
+                    <RenderDish dish={props.dish} />
+                    <RenderComments comments={props.comments} />
+                </div>
+                          
             </div>
         );
     }
