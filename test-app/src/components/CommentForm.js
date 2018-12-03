7ff94c2e45bc0177e2ed442441db5691b32963cc
henos tekle <mk22005@hotmail.com>
2018-12-04 00:45:38 +0100

    Thrid assignment
    done

--- test-app/src/components/CommentForm.js
+++ test-app/src/components/CommentForm.js
@@ -24,61 +24,56 @@ class CommentForm extends Component {
         });
       }
 
+    handleSubmit(values) {
+        alert("Current State is: " + JSON.stringify(values));
+    }
+
 
     render() {
         
         return (
         <div>
             <Button outline onClick={this.toggleModal}>
-                <span className="fa fa-sign-in fa-lg">Submit Comment</span>
+                <span className="fa fa-pencil"> Submit Comment</span>
             </Button>   
             <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
                 <ModalHeader toggle={this.toggleModal}>Submit Comment</ModalHeader>
-                    <ModalBody>
-                            <LocalForm onSubmit={(values) => this.handleSubmit(values)}>
-                                <Row className="form-group">
-                                    <Label htmlFor="rating">Rating</Label>                                    
-                                    <Control.select  model=".rating" name="rating"
-                                        className="form-control">
-                                        <option>1</option>
-                                        <option>2</option>
-                                        <option>3</option>
-                                        <option>4</option>
-                                        <option>5</option>
-                                        <option>6</option>
-                                        <option>7</option>
-                                        <option>8</option>    
-                                    </Control.select>                                  
-                                </Row>
-                                <Row>
-                                    <Label htmlFor="name">Your name</Label>
-                                    <Control.text model=".name" id="name" name="name"
-                                      placeholder="name"
-                                      className="form-control"
-                                      validators={{
-                                          required, minLength: minLength(3), maxLength: maxLength(15)
-                                      }} 
-                                    />
-                                    <Errors
-                                        className="text-danger"
-                                        model=".name"
-                                        show="touched"
-                                        messages={{
-                                            required: 'Required',
-                                            minLength: 'Must be greater than 2 characters',
-                                            maxLength: 'Must be 15 characters or less'
-                                        }}
-                                    />
-                                </Row>
-                                <Row className="form-group">
-                                    <Label htmlFor="username">Comment</Label>
-                                    <Control.textarea model=".comment" id="comment" name="comment"
-                                        rows="6"
-                                        className="form-control" />                                   
-                                </Row>
-                                <Button type="submit" color="primary">Submit</Button>
-                            </LocalForm>
-                        </ModalBody>
+                <ModalBody>
+                    <LocalForm onSubmit={(values) => this.handleSubmit(values)}>                                
+                        <Label htmlFor="rating">Rating</Label>                                    
+                        <Control.select  model=".rating" name="rating"
+                            className="form-control">
+                            <option>1</option>
+                            <option>2</option>
+                            <option>3</option>
+                            <option>4</option>
+                            <option>5</option>                                         
+                        </Control.select>                              
+                        <Label htmlFor="name" className="mt-2">Your name</Label>
+                        <Control.text model=".author" id="author" name="author"
+                            placeholder="author"
+                            className="form-control"
+                            validators={{
+                                required, minLength: minLength(3), maxLength: maxLength(15)
+                            }} 
+                        />
+                        <Errors
+                            className="text-danger"
+                            model=".author"
+                            show="touched"
+                            messages={{
+                                required: 'Required',
+                                minLength: 'Must be greater than 2 characters',
+                                maxLength: 'Must be 15 characters or less'
+                            }}
+                        />
+                        <Label htmlFor="username" className="mt-2">Comment</Label>
+                        <Control.textarea model=".comment" id="comment" name="comment"
+                            rows="6"
+                            className="form-control" />     
+                        <Button type="submit" className="mt-2" color="primary">Submit</Button>
+                    </LocalForm>
+                </ModalBody>
             </Modal>         
         </div>
         )
